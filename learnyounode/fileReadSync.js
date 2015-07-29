5dc93339d47bf7113c22413d400c935d0f90c4f2
dotkay <mensch@live.in>
2015-07-28 23:47:10 -0700

    Ex.3 Using fileReadSync to read a file and counting the number of lines in it

---
+++ learnyounode/fileReadSync.js
@@ -0,0 +1,21 @@
+// fileReadSync.js
+
+var fs = require('fs')
+
+// The path to the file to be read is given as the first
+// command-line argument, which is actually process.argv[2]
+// The first argument is node (process.argv[0]) and the
+// second is the file name of this program - fileReadSync.js
+// (process.argv[1]
+
+// toString() on process.argv[2] converts the argument into a 
+// string. I think process.argv returns a string and so this 
+// is actually not required
+
+var data = fs.readFileSync(process.argv[2].toString())
+// Again, .toString() converts the data into a string
+// split(x) splits the string using the marker 'x' and
+// fills the resulting pieces in an array whose length
+// is obtained by .length()
+var num_lines = data.toString().split('\n').length - 1
+console.log(num_lines)
