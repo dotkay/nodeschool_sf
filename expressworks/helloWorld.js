1a7d11d232dc7417d1000ba3bf9cfd580f53cdbc
dotkay <mensch@live.in>
2015-08-04 22:56:23 -0700

    Ex.1. A simple program to illustrate Express

---
+++ expressworks/helloWorld.js
@@ -0,0 +1,9 @@
+// helloWorld.js
+
+var express = require('express')
+var app = express()
+
+var port = process.argv[2]
+app.get('/home', function(req, res) {
+	res.end('Hello World!')
+}).listen(port)
