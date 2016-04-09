6d8d965baf9e0c3e1c464bbad4f8452af27551e3
kay <kalyanvk@gmail.com>
2016-04-09 14:35:24 -0700

    Update commandLine.js
    added notes

--- learnyounode/commandLine.js
+++ learnyounode/commandLine.js
@@ -1,6 +1,10 @@
 // commandLine.js
 
 var sum = 0
+// you run this exercise as node commandLine.js arg1 arg2...
+// var = 0: indicates the 'node' program
+// var = 1: indicates this filename
+// var = 2, ... are the commandline arguments
 for (var i = 2; i < process.argv.length; i++) {
 	sum += Number(process.argv[i])
 }
