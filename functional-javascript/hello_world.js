d16480a43c779ff98f3c72fe9049438fa2e853a2
kalyan <kalyan>
2016-05-06 22:59:08 -0700

    Ex.1. Introduction - functions in JavaScript

---
+++ functional-javascript/hello_world.js
@@ -0,0 +1,9 @@
+// hello_world.js
+// Write a function that takes an input string as argument and
+// returns it uppercased
+
+function upperCaser (input) {
+  return input.toUpperCase();
+};
+
+module.exports = upperCaser;
